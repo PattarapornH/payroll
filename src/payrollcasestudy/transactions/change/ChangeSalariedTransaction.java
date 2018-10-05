844503b459c8c0cc2297ae76da70c2520ecd8330
PattarapornH <pataraporn.han@gmail.com>
2018-10-05 15:22:44 +0700

    can run test

--- src/payrollcasestudy/transactions/change/ChangeSalariedTransaction.java
+++ src/payrollcasestudy/transactions/change/ChangeSalariedTransaction.java
@@ -1,8 +1,8 @@
 
 package payrollcasestudy.transactions.change;
 
-import payrollcasestudy.entities.payentclassifications.SalariedClassification;
 import payrollcasestudy.entities.paymentclassifications.PaymentClassification;
+import payrollcasestudy.entities.paymentclassifications.SalariedClassification;
 import payrollcasestudy.entities.paymentschedule.MonthlyPaymentSchedule;
 import payrollcasestudy.entities.paymentschedule.PaymentSchedule;
 
